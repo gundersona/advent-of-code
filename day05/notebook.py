da4990722478e9586ef72ff4bb98d8d550047bcc
gundersona <acgunderson@gmail.com>
2026-02-22 17:12:13 -0600

    Attempt day 5 part 2. Example solution correct, actual solution incorrect

--- day05/notebook.py
+++ day05/notebook.py
@@ -1,7 +1,7 @@
 import marimo
 
-__generated_with = "0.19.8"
-app = marimo.App(width="medium")
+__generated_with = "0.19.11"
+app = marimo.App()
 
 
 @app.cell(hide_code=True)
@@ -101,6 +101,87 @@ def _(mo):
 @app.cell
 def _(input_available_ingredients, input_fresh_ranges):
     count_fresh_ids(input_fresh_ranges, input_available_ingredients)
+    return
+
+
+@app.cell
+def _():
+    example_solution_2 = 14
+    return (example_solution_2,)
+
+
+@app.function
+def count_all_fresh_ids(id_ranges):
+    ids = 0
+    for id_range in id_ranges:
+        lower, upper = id_range.split("-")
+        ids += int(upper) - int(lower) + 1
+    return ids
+
+
+@app.function
+def count_fresh_id_duplicates(id_ranges):
+    ids = 0
+    ranges = [
+        row for row in id_ranges
+    ]  # deep copy to avoid modifying original
+    for i, id_range in enumerate(ranges):
+        lower, upper = map(int, id_range.split("-"))
+        for id_range_to_compare in ranges[i + 1 :]:
+            lower_compare, upper_compare = map(
+                int, id_range_to_compare.split("-")
+            )
+            # Case 1: entire range overlaps, nothing unique
+            if lower_compare >= lower and upper_compare <= upper:
+                ids += upper_compare - lower_compare + 1
+                ranges.remove(id_range_to_compare)
+            # Case 2: top part of range overlaps, bottom part unique
+            elif (
+                lower_compare < lower
+                and upper_compare <= upper
+                and upper_compare >= lower
+            ):
+                ids += upper_compare - lower + 1
+                ranges[i] = f"{lower_compare}-{lower - 1}"
+            # Case 3: bottom part of range overlaps, top part unique
+            elif (
+                upper_compare > upper
+                and lower_compare >= lower
+                and lower_compare <= upper
+            ):
+                ids += upper - lower_compare + 1
+                ranges[i] = f"{upper + 1}-{upper_compare}"
+            # Case 4: entire range overlaps, top and bottom part unique
+            elif lower_compare < lower and upper_compare > upper:
+                ids += upper - lower + 1
+                ranges[i] = f"{lower_compare}-{lower - 1}"
+                ranges.append(f"{upper + 1}-{upper_compare}")
+
+    return ids
+
+
+@app.function
+def count_all_fresh_ids_no_duplicates(id_ranges):
+    return count_all_fresh_ids(id_ranges) - count_fresh_id_duplicates(
+        id_ranges
+    )
+
+
+@app.cell
+def _(example_fresh_ranges):
+    count_all_fresh_ids_no_duplicates(example_fresh_ranges)
+    return
+
+
+@app.cell
+def _(example_fresh_ranges, example_solution_2):
+    count_all_fresh_ids_no_duplicates(example_fresh_ranges) == example_solution_2
+    return
+
+
+@app.cell
+def _(input_fresh_ranges):
+    count_all_fresh_ids_no_duplicates(input_fresh_ranges)
     return
 
 
